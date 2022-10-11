30642a52a10afb865de5e7a319d4bf192ac6d9ca
heuzo <vlad.neight21@gmail.com>
2022-10-11 18:20:26 +0300

    Secured secret key in ini

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -9,18 +9,18 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import configparser
 import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+# Create object of configparser to read ini file
+config = configparser.ConfigParser()
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wtkkcypy+pvu#2jo8)zf)#)3*4_3rs69z0heyhx&bk#=dmx&1p'
+SECRET_KEY = config['KEYS']['Django_secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
